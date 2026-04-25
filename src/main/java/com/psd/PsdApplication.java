cf4c5e461ffb7eccd67385b25a57272437951818
Рафит Акчурин <akrafit@gmail.com>
2026-04-25 22:06:14 +0500

    Create PSD

---
+++ src/main/java/com/psd/PsdApplication.java
@@ -0,0 +1,13 @@
+package com.psd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsdApplication.class, args);
+	}
+
+}
